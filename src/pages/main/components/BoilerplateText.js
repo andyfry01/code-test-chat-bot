9118f92e843216de4a503c3160c787b15b9b037a
Andy Fry <andyfry01@gmail.com>
2018-02-16 13:44:28 -0500

    feat: add boilerplate component/markdown

---
+++ src/pages/main/components/BoilerplateText.js
@@ -0,0 +1,27 @@
+import React  from 'react';
+
+const BoilerplateText = () => {
+  return (
+    <div className="boilerplate">
+      <h1 className="boilerplate__header">Bob Ross Lorem</h1>
+      <p className="boilerplate__paragraph">You have to make those little noises or it won't work. Paint anything you want on the canvas. Create your own world. Maybe there's a happy little waterfall happening over here. This is an example of what you can do with just a few things, a little imagination and a happy dream in your heart. In nature, dead trees are just as normal as live trees.</p>
+      <p className="boilerplate__paragraph">Isn't that fantastic that you can create an almighty tree that fast? Those great big fluffy clouds. Let's make a happy little mountain now.</p>
+      <h2 className="boilerplate__header">Happy Trees</h2>
+      <p className="boilerplate__paragraph">So often we avoid running water, and running water is a lot of fun. If I paint something, I don't want to have to explain what it is. Let's put some happy little bushes on the other side now.</p>
+      <ul className="boilerplate__list">
+        <li className="boilerplate__listItem">Lorem</li>
+        <li className="boilerplate__listItem">Lorem ipsum sit amet</li>
+        <li className="boilerplate__listItem">Dolor</li>
+      </ul>
+      <p className="boilerplate__paragraph">For the lack of a better word I call them hangy downs. We wash our brush with odorless thinner. The light is your friend. Preserve it. This is truly an almighty mountain. No worries. No cares. Just float and wait for the wind to blow you around.</p>
+      <p className="boilerplate__paragraph">Let your imagination be your guide. Automatically, all of these beautiful, beautiful things will happen. All you have to learn here is how to have fun. Let's put some happy little clouds in our world.</p>
+      <p className="boilerplate__paragraph">Talk to trees, look at the birds. Whatever it takes. You can do anything here - the only pre-requisite is that it makes you happy. It's amazing what you can do with a little love in your heart. All you need to paint is a few tools, a little instruction, and a vision in your mind. Let's give him a friend too. Everybody needs a friend.</p>
+      <p className="boilerplate__paragraph">If we're gonna walk though the woods, we need a little path. Maybe there's a happy little bush that lives right there. Any little thing can be your friend if you let it be. Everyone is going to see things differently - and that's the way it should be.</p>       
+    </div>
+  );
+}
+
+export default BoilerplateText;
+
+
+
