c604b022a112c920cf1a841245d6311146d5a5ad
Andy Fry <andyfry01@gmail.com>
2018-02-16 18:46:01 -0500

    feat: add chatbox open and close fxnality

--- src/pages/main/components/ChatBot.js
+++ src/pages/main/components/ChatBot.js
@@ -11,13 +11,28 @@ class ChatBot extends Component {
     this.state = {
       chatBotOpen: true
     }
+    this.toggleChatWindow = this.toggleChatWindow.bind(this)
+  }
+  toggleChatWindow(chatWindowStatus){
+    if (chatWindowStatus === 'closed') {
+      return this.setState(state => {
+        return {
+          chatBotOpen: false
+        }
+      })
+    }
+    return this.setState(state => {
+      return {
+        chatBotOpen: true
+      }
+    })
   }
   render() {
     if (!this.state.chatBotOpen) {
       return (
         <div className="chatBotContainer">
           <div className="chatBot__main --closed">
-            <ChatBotHeader />
+            <ChatBotHeader toggleChatWindow={this.toggleChatWindow} />
             <ChatBotChatWindow />
           </div>
         </div>
@@ -26,7 +41,7 @@ class ChatBot extends Component {
     return (
       <div className="chatBotContainer">
         <div className="chatBot__main --open">
-          <ChatBotHeader />
+          <ChatBotHeader toggleChatWindow={this.toggleChatWindow} />
           <ChatBotChatWindow />
         </div>
       </div>
--- src/pages/main/components/ChatBotHeader.js
+++ src/pages/main/components/ChatBotHeader.js
@@ -1,19 +1,52 @@
 // React
-import React from 'react';
+import React, {Component} from 'react';
 
 // Assets
 import x from '../../../assets/x.svg'
 import chevronUp from '../../../assets/chevron-up.svg'
 
-const ChatBotHeader = () => {
-  return (
-    <div className="chatbot__header">
-      <p className="chatbot__headerText">Chat</p>
-      <div className="chatbot__toggler">
-        <img className="chatbot__togglerIcon" src={x} alt="chatbot open and close button" />
+class ChatBotHeader extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true,
+      icon: x
+    }
+    this.toggleChatHeader = this.toggleChatHeader.bind(this)
+  }
+
+  toggleChatHeader() {
+    if (this.state.open === false) {
+      this.props.toggleChatWindow('open')
+      return this.setState(state => {
+        return {
+          open: true,
+          icon: x
+        }
+      })
+    }
+    this.props.toggleChatWindow('closed')
+    return this.setState(state => {
+      return {
+        open: false,
+        icon: chevronUp
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div className="chatbot__header">
+        <p className="chatbot__headerText">Chat</p>
+        <div className="chatbot__toggler">
+          <img  className="chatbot__togglerIcon" 
+                src={this.state.icon} 
+                alt="chatbot open and close button" 
+                onClick={this.toggleChatHeader}/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ChatBotHeader;
