49cbea2e8b50a42de39c97a80bda44d61e8d4930
Andy Fry <andyfry01@gmail.com>
2018-02-16 17:14:03 -0500

    feat: add chat window markup

--- src/pages/main/components/ChatBotChatWindow.js
+++ src/pages/main/components/ChatBotChatWindow.js
@@ -3,7 +3,18 @@ import React from 'react';
 
 const ChatBotChatWindow = () => {
   return (
-    <div className="chatbot__chatWindow">Hi from the chat window</div>
+    <div className="chatWindow">
+      <div className="chatWindow__companyMessage">Hi! Do you have any questions I can answer for you?</div>
+      <div className="chatWindow__chatOptions">
+        <div className="chatWindow__notNow">Not now</div>
+        <div className="chatWindow__yesPlease">Yes please</div>
+      </div>
+      <div className="typingIndicator">
+        <div className="typingIndicator__ellipse"></div>
+        <div className="typingIndicator__ellipse"></div>
+        <div className="typingIndicator__ellipse"></div>
+      </div>
+    </div>
   );
 }
 
