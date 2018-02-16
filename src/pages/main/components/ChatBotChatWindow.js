78143988bc06c5831b155246e435a8de88f4b7fb
Andy Fry <andyfry01@gmail.com>
2018-02-16 18:14:22 -0500

    refactor: remove typing indicator from chat window component

--- src/pages/main/components/ChatBotChatWindow.js
+++ src/pages/main/components/ChatBotChatWindow.js
@@ -1,19 +1,18 @@
 // React
 import React from 'react';
 
+// Components
+import TypingIndicator from './TypingIndicator'
+
 const ChatBotChatWindow = () => {
   return (
     <div className="chatWindow">
       <div className="chatWindow__companyMessage">Hi! Do you have any questions I can answer for you?</div>
       <div className="chatWindow__chatOptions">
         <div className="chatWindow__notNow">Not now</div>
-        <div className="chatWindow__yesPlease">Yes please</div>
+        <div className="cbhatWindow__yesPlease">Yes please</div>
       </div>
-      <div className="typingIndicator">
-        <div className="typingIndicator__ellipse"></div>
-        <div className="typingIndicator__ellipse"></div>
-        <div className="typingIndicator__ellipse"></div>
-      </div>
+      <TypingIndicator animationInterval={500} />
     </div>
   );
 }
