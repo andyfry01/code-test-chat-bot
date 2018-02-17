47f137598e208c574252f885c6e853ebaa219c60
Andy Fry <andyfry01@gmail.com>
2018-02-17 10:10:08 -0500

    feat: add user input and plain message components

---
+++ src/pages/main/components/PlainMessage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PlainMessage = (props) => {
+  return <div className={props.className}>{props.copy}</div>
+}
+
+export default PlainMessage
---
+++ src/pages/main/components/UserInputGetter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserInputGetter = (props) => {
+  return (
+    <div className={props.className} onClick={props.handleClick}>{props.copy}</div>
+  )
+} 
+
+export default UserInputGetter
