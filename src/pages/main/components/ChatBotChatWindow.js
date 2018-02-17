de305ed9f2a46ca13bfcdc6f56a05327396bbdbc
Andy Fry <andyfry01@gmail.com>
2018-02-17 11:20:03 -0500

    refactor: refactor script execution methods
    organized methods into smaller functions for readability, renamed variables to be more semantic,
    moved script info into separate module

--- src/pages/main/components/ChatBotChatWindow.js
+++ src/pages/main/components/ChatBotChatWindow.js
@@ -6,123 +6,135 @@ import TypingIndicator from './TypingIndicator'
 import UserInputGetter from './UserInputGetter'
 import PlainMessage from './PlainMessage'
 
+// Chatbot script
+import chatScript from '../etc/chatScript'
+
 let chatTimeoutId = undefined
 
 class ChatBotChatWindow extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      messages: {
-        message1: 'Great! We\'ll get you connected to a representative',
-        message2: 'No problem! If you decide you need me, just click "Contact Us"'
-      },
-      executionPaths: {
-        affirmative: [{ name: 'showIndicator', duration: 3500}, 
-                      { name: 'showMessage1', duration: 4000}, 
-                      { name: 'showIndicator', duration: 3500}, 
-                      { name: 'closeWindow', duration: null}],
-
-          negative:  [{ name: 'showMessage2', duration: 1500}, 
-                      { name: 'displayFollowUpPrompt', duration: null}]
-      },
-      executionPathsFast: {
-        affirmative: [{ name: 'showIndicator', duration: 500}, 
-                      { name: 'showMessage1', duration: 500}, 
-                      { name: 'showIndicator', duration: 500}, 
-                      { name: 'closeWindow', duration: null}],
-
-          negative:  [{ name: 'showMessage2', duration: 500}, 
-                      { name: 'displayFollowUpPrompt', duration: null}]
-      },
-      currentExecutionPath: undefined,
+      currentScriptPath: undefined,
       chatMessages: []
     }
     this.answerPrompt = this.answerPrompt.bind(this)
-    this.stepExecutionPath = this.stepExecutionPath.bind(this)
-    this.triggerExecutionStep = this.triggerExecutionStep.bind(this)
+    this.stepScriptPath = this.stepScriptPath.bind(this)
+    this.triggerScriptStep = this.triggerScriptStep.bind(this)
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (nextState.currentExecutionPath !== this.state.currentExecutionPath) {
-      this.stepExecutionPath(nextState.currentExecutionPath, -1)
+  componentWillUpdate(nextProps, nextState) {
+    if (nextState.currentScriptPath !== this.state.currentScriptPath) {
+      this.stepScriptPath(nextState.currentScriptPath, -1)
     }
     return true
   }
 
   answerPrompt(answer) {
     if (answer === 'no') {
-      return this.setState(state => {
+      this.setState(state => {
         return {
-          currentExecutionPath: 'negative'
+          currentScriptPath: 'negative'
         }
       })
+      return
     } 
-    if (answer === 'yes') {
-      return this.setState(state => {
-        return {
-          currentExecutionPath: 'affirmative'
-        }
-      })
-    }
-  }
-
-  stepExecutionPath(pathName, stepNum) {
-    window.clearTimeout(chatTimeoutId)
-    let nextStep = stepNum + 1
-    this.triggerExecutionStep(pathName, nextStep)
-  }
-
-  triggerExecutionStep(pathName, stepNum) {
-    let chatMessages = this.state.chatMessages
-    if (chatMessages.length > 0) {
-      chatMessages = chatMessages.filter(chatItem => chatItem.type.name !== 'TypingIndicator')
-    }
-    let curState = this.state
-    let executionSteps = curState.executionPaths[pathName]
-    let currentExecutionStep = executionSteps[stepNum]
-    if (currentExecutionStep === undefined) {
-      window.clearTimeout(chatTimeoutId)
-      return false
-    } 
-    let stepName = currentExecutionStep.name
-    let nextMessage = undefined
-
-    if (stepName === 'showIndicator') {
-      nextMessage = <TypingIndicator key={Date.now() + 1} animationInterval={500} />
-    } 
-    if (stepName === 'showMessage1') {
-      nextMessage = <PlainMessage className="chatWindow__companyMessage" copy={this.state.messages.message1}/>
-    }
-    if (stepName === 'showMessage2') {
-      nextMessage = <PlainMessage className="chatWindow__companyMessage" copy={this.state.messages.message2}/>
-    }
-    if (stepName === 'displayFollowUpPrompt') {
-      nextMessage = <div className="chatWindow__chatOptions"> 
-                      <UserInputGetter  key={Date.now()} 
-                                        className="chatWindow__affirmativeOption" 
-                                        copy="Actually, I changed my mind" 
-                                        handleClick={() => { this.answerPrompt('yes') }} /> 
-                    </div>
-    }
-    if (stepName === 'closeWindow') {
-      this.props.toggleChatWindow()
-      window.clearTimeout(chatTimeoutId)
-      return false
-    }
-    chatMessages.push(nextMessage)
     this.setState(state => {
       return {
-        chatMessages: chatMessages
+        currentScriptPath: 'affirmative'
       }
-    }, () => {
-      let chatWindow = document.getElementsByClassName("chatWindow")[0];
-      chatWindow.scrollTop = chatWindow.scrollHeight;
-    }) 
-    chatTimeoutId = window.setTimeout(() => { this.stepExecutionPath(pathName, stepNum)}, executionSteps[stepNum].duration)
+    })
+    return
+  }
+
+  stepScriptPath(pathName, stepNum) {
+    window.clearTimeout(chatTimeoutId)
+    let nextStep = stepNum + 1
+    this.triggerScriptStep(pathName, nextStep)
+  }
+
+  triggerScriptStep(pathName, stepNum) {
+
+    let chatMessages = this.state.chatMessages
+    let chatScriptSteps = chatScript.scriptPathsFast[pathName]
+    let currentScriptStep = chatScriptSteps[stepNum]
+
+    if (chatScriptIsNotOver(currentScriptStep, this)) {
+
+      let stepName = currentScriptStep.name
+      let stepDuration = currentScriptStep.duration
+      let nextMessage = undefined
+
+      if (chatMessages.length > 0) {
+        chatMessages = removeTypingIndicator(chatMessages)
+      }
+      if (stepName === 'showIndicator') {
+        nextMessage = <TypingIndicator key={Date.now()} animationInterval={500} />
+      } 
+      if (stepName === 'showMessage1') {
+        nextMessage = <PlainMessage key={Date.now()} 
+                                    className="chatWindow__companyMessage" 
+                                    copy={chatScript.messages.message1} />
+      }
+      if (stepName === 'showMessage2') {
+        nextMessage = <PlainMessage key={Date.now()} 
+                                    className="chatWindow__companyMessage" 
+                                    copy={chatScript.messages.message2} />
+      }
+      if (stepName === 'displayFollowUpPrompt') {
+        nextMessage = <div className="chatWindow__chatOptions" key={Date.now()} > 
+                        <UserInputGetter  className="chatWindow__affirmativeOption" 
+                                          copy="Actually, I changed my mind" 
+                                          handleClick={() => { this.answerPrompt('yes') }} /> 
+                      </div>
+      }
+      if (nextMessage !== undefined) {
+        chatMessages.push(nextMessage)
+      }
+
+      this.setState(state => {
+        return {
+          chatMessages: chatMessages
+        }
+      }) 
+      scrollChatWindow()
+      chatTimeoutId = window.setTimeout(() => { 
+        this.stepScriptPath(pathName, stepNum)
+      }, stepDuration)
+    }
+
+    function scrollChatWindow(){
+      let chatWindow = document.getElementsByClassName("chatWindow")[0]
+      chatWindow.scrollTop = chatWindow.scrollHeight
+    }
+
+    function removeTypingIndicator(chatMessages) {
+      return chatMessages.filter(chatItem => chatItem.type.name !== 'TypingIndicator')
+    }
+    
+    function chatScriptIsNotOver(currentScriptStep, that){
+      if (currentScriptStep === undefined) {
+        window.clearTimeout(chatTimeoutId)
+        return false
+      }
+      if (currentScriptStep.name === 'closeWindow') {
+        chatMessages = removeTypingIndicator(chatMessages)
+        that.setState(state => {
+          return {chatMessages: chatMessages}
+        }, () => {
+          that.props.toggleChatWindow()
+          window.clearTimeout(chatTimeoutId)
+          return false
+        })
+      } 
+      return true
+    }
+
   }
 
   render(){
-    // accommondate two messages already in chat window
+    // used to move chat spacer on addition of new message
+    // add 2 to accommondate two messages already in chat window
     let chatListLength = this.state.chatMessages.length + 2
     return (
       <div className="chatWindow">
@@ -137,7 +149,7 @@ class ChatBotChatWindow extends Component {
                             handleClick={() => { this.answerPrompt('yes') }} />
         </div>
         {this.state.chatMessages.map(message => message)}
-        <div className="chatWindow__spacer" style={{gridRowStart: `${chatListLength + 1}`, gridRowEnd: `${chatListLength + 2}`}}></div>
+        <div className="chatWindow__spacer" style={{ gridRow: `${chatListLength + 1} / ${chatListLength + 2}` }}></div>
       </div>
     );
   }
